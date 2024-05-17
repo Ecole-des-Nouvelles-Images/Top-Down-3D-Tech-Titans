1adb04f1b4f4d6ef93e28a10c211cf64782439d6
SCLKriss <117286300+SCLKskellka@users.noreply.github.com>
2024-05-17 17:31:15 +0200

    intégration des modèle importé par Maxime

--- Assets/Christopher/Scripts/Modules/OxygenModule.cs
+++ Assets/Christopher/Scripts/Modules/OxygenModule.cs
@@ -14,7 +14,22 @@ public class OxygenModule : SubmarinModule
     // Update is called once per frame
     void Update()
     {
-        
+        if (!IsActivated)
+        {
+            if(States.Count > 0 && States[0] != null){ 
+                foreach (GameObject obj in StateDisplayObject) {
+                    obj.transform.GetComponent<MeshRenderer>().material = States[0];
+                }
+            }
+        }
+        if (IsActivated)
+        {
+            if(States.Count > 0 && States[1] != null){ 
+                foreach (GameObject obj in StateDisplayObject) {
+                    obj.transform.GetComponent<MeshRenderer>().material = States[1];
+                }
+            }
+        }
     }
 
     public override void Activate()
