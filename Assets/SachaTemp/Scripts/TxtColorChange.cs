7315f6551cff32aaa0c0fe38a60fa7cb836c0b13
Tartacule <s.nino@nouvellesimages.xyz>
2024-06-05 13:52:12 +0200

    Modified death screen and pause screen

--- Assets/SachaTemp/Scripts/TxtColorChange.cs
+++ Assets/SachaTemp/Scripts/TxtColorChange.cs
@@ -9,22 +9,54 @@ public class ButtonColorChange : MonoBehaviour, IPointerEnterHandler, IPointerEx
     public TextMeshProUGUI buttonText;
     public Color normalTextColor = Color.green;
     public Color highlightedTextColor = Color.black;
+    public AudioClip highlightSound; 
+    private AudioSource audioSource; 
 
     void Start()
     {
         button = GetComponent<Button>();
         buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+        audioSource = GetComponent<AudioSource>();
+
+       
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
         ResetColors();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         buttonText.color = highlightedTextColor;
+        PlayHighlightSound();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         ResetColors();
+    }
+
+    
+    public void OnPointerEnter(BaseEventData eventData)
+    {
+        buttonText.color = highlightedTextColor;
+        PlayHighlightSound(); 
+    }
+
+    public void OnPointerExit(BaseEventData eventData)
+    {
+        ResetColors();
+    }
+
+    private void PlayHighlightSound()
+    {
+       
+        if (highlightSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(highlightSound);
+        }
     }
 
     private void ResetColors()
